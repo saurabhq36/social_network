1e2fee4a653b618a861ac0d38a7932905a611be4
Saurabh <saurabh@Saurabhs-MacBook-Air.local>
2024-08-28 09:30:57 +0530

    changes in urls and added docstring

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import SignupView, LoginView,UserSearchView,FriendRequestView,ListFriendsView,ListPendingFriendRequestsView
 app_name = 'users'
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('search/', UserSearchView.as_view(), name='user_search'),
-    path('friend-request/', FriendRequestView.as_view(), name='friend_request'),
-    path('friends/', ListFriendsView.as_view(), name='friends_list'),
-    path('pending-requests/', ListPendingFriendRequestsView.as_view(), name='pending_requests')
+    path('signup', SignupView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login'),
+    path('user_search/', UserSearchView.as_view(), name='user_search'),
+    path('friend-request', FriendRequestView.as_view(), name='friend_request'),
+    path('friends_list', ListFriendsView.as_view(), name='friends_list'),
+    path('pending-recieved-requests', ListPendingFriendRequestsView.as_view(), name='pending_requests')
 ]
--- users/views.py
+++ users/views.py
@@ -81,6 +81,35 @@ class LoginView(APIView):
 
 
 class UserSearchView(APIView):
+    """
+    API endpoint for searching users by first name, last name, or email.
+
+    This view allows authenticated users to search for other users in the system.
+    The search can be performed using a keyword, which can be either a part of the
+    first name, last name, or the exact email address of the user.
+
+    Pagination is applied to the search results to limit the number of users returned in a single response.
+
+    Attributes:
+        permission_classes (list): Specifies that only authenticated users can access this view.
+        pagination_class (class): Defines the pagination class used to paginate the search results.
+        serializer_class (class): Specifies the serializer class used to serialize the user data.
+
+    Methods:
+        get(self, request, *args, **kwargs):
+            Handles GET requests to search for users based on the provided keyword.
+
+            Input:
+                - request (Request): The HTTP request object. The `search` keyword is expected in the query parameters.
+
+            Output:
+                - If the search keyword is missing:
+                    - Returns a JSON response with an error message and a 400 HTTP status.
+                - If no users are found matching the search criteria:
+                    - Returns a JSON response with an error message and a 404 HTTP status.
+                - If users are found:
+                    - Returns a paginated JSON response containing the serialized user data and a 200 HTTP status.
+    """
     permission_classes = [IsAuthenticated]
     pagination_class = UserSearchPagination
     serializer_class = UserSerializer
@@ -115,11 +144,61 @@ class UserSearchView(APIView):
 
 
 class FriendRequestView(APIView):
+    """
+    API endpoint for managing friend requests.
+
+    This view allows authenticated users to send, accept, or reject friend requests
+    to other users. The actions are determined based on the 'action' parameter sent
+    in the request body.
+
+    Attributes:
+        permission_classes (list): Specifies that only authenticated users can access this view.
+
+    Methods:
+        post(self, request):
+            Handles POST requests to manage friend requests.
+
+            Input:
+                - request (Request): The HTTP request object containing the following data in the body:
+                    - action (str): The action to perform, which can be 'send', 'accept', or 'reject'.
+                    - target_email (str): The email of the user to whom the friend request is being sent, accepted, or rejected.
+
+            Output:
+                - If 'action' or 'target_email' is missing:
+                    - Returns a JSON response with an error message and a 400 HTTP status.
+                - If the target user is not found:
+                    - Returns a JSON response with an error message and a 404 HTTP status.
+                - If the user attempts to send a friend request to themselves:
+                    - Returns a JSON response with an error message and a 400 HTTP status.
+
+                - If 'send' action is chosen:
+                    - If the user has already sent 3 requests within the last minute:
+                        - Returns a JSON response with an error message and a 429 HTTP status.
+                    - If there is an existing pending or accepted friend request:
+                        - Returns a JSON response with an appropriate message and a 400 HTTP status.
+                    - If no issues are found:
+                        - Creates a new friend request, updates the request count, and returns a JSON response with a 200 HTTP status.
+
+                - If 'accept' action is chosen:
+                    - If there is no pending request from the target user:
+                        - Returns a JSON response with an error message and a 400 HTTP status.
+                    - If a pending request is found:
+                        - Accepts the request, creates a friendship, and returns a JSON response with a 200 HTTP status.
+
+                - If 'reject' action is chosen:
+                    - If there is no pending request from the target user:
+                        - Returns a JSON response with an error message and a 400 HTTP status.
+                    - If a pending request is found:
+                        - Rejects the request and returns a JSON response with a 200 HTTP status.
+
+                - If an invalid action is provided:
+                    - Returns a JSON response with an error message and a 400 HTTP status.
+    """
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
         action = request.data.get('action')
-        target_email = request.data.get('email')
+        target_email = request.data.get('target_email')
         user = request.user
         
         if not action or not target_email:
@@ -192,6 +271,29 @@ class FriendRequestView(APIView):
 
 
 class ListFriendsView(APIView):
+    """
+    API endpoint to list all friends of the authenticated user.
+
+    This view retrieves the list of friends for the authenticated user by checking both
+    sides of the Friendship relationship (i.e., where the user is either user1 or user2).
+    The friends are then returned in descending order based on the date they joined.
+
+    Attributes:
+        permission_classes (list): Specifies that only authenticated users can access this view.
+
+    Methods:
+        get(self, request, *args, **kwargs):
+            Handles GET requests to list the friends of the authenticated user.
+
+            Input:
+                - request (Request): The HTTP request object that contains user authentication.
+
+            Output:
+                - If the user has no friends:
+                    - Returns a JSON response with the message "No friends yet" and a 200 HTTP status.
+                - If the user has friends:
+                    - Returns a serialized list of friends, ordered by the date they joined, and a 200 HTTP status.
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
@@ -214,6 +316,8 @@ class ListFriendsView(APIView):
         
         # Serialize and return the friends
         serializer = UserSerializer(friends, many=True)
+        if not serializer.data:
+            return Response({"message": "No friends yet"}, status=status.HTTP_200_OK)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
